461cdc8bee1a55b63d430a5553a1d5a6ae57af20
phi1ip83 <philip.dossantos@hotmail.com>
2023-03-12 18:27:32 -0400

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ReadyToPayChangeResponse } from '@google-pay/button-angular';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,64 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pay';
+
+  amount = '100.00';
+  buttonType:any = 'buy';
+  buttonColor:any = 'default';
+  buttonLocale = '';
+  existingPaymentMethodRequired = false;
+
+  paymentRequest:any = {
+    apiVersion: 2,
+    apiVersionMinor: 0,
+    allowedPaymentMethods: [
+      {
+        type: 'CARD',
+        parameters: {
+          allowedAuthMethods: ['PAN_ONLY', 'CRYPTOGRAM_3DS'],
+          allowedCardNetworks: ['MASTERCARD', 'VISA'],
+        },
+        tokenizationSpecification: {
+          type: 'PAYMENT_GATEWAY',
+          parameters: {
+            gateway: 'example',
+            gatewayMerchantId: 'exampleGatewayMerchantId',
+          },
+        },
+      },
+    ],
+    merchantInfo: {
+      merchantId: '12345678901234567890',
+      merchantName: 'Demo Merchant',
+    },
+  };
+
+  onLoadPaymentData = (event: any): void => {
+    console.log('load payment data', event.detail);
+  };
+
+  onError = (event: ErrorEvent): void => {
+    console.error('error', event.error);
+  };
+
+  onPaymentDataAuthorized: google.payments.api.PaymentAuthorizedHandler = paymentData => {
+    console.log('payment authorized', paymentData);
+
+    return {
+      transactionState: 'SUCCESS',
+    };
+  };
+
+  onReadyToPayChange = (event: any): void => {
+    console.log('ready to pay change', event.detail);
+  };
+
+  onClick = (event: Event): void => {
+    console.log('click');
+  };
+
+  onClickPreventDefault = (event: Event): void => {
+    console.log('prevent default');
+    event.preventDefault();
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from "@angular/forms";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GooglePayButtonModule } from '@google-pay/button-angular';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GooglePayButtonModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
